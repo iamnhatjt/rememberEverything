687be99e4a7da326cff23e3a3a3f97042804b15b
NhatJt vn <iamnhatjt@gmail.com>
2021-10-09 16:29:09 +0700

    change data.json

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Body from './bodyWeb/body'
 import './style/app.scss'
 import Navbar from './Tcomponent/navbar/navbar.js'
 import { BrowserRouter as Router } from 'react-router-dom'
+import data from"./data.json"
 
 const App = () => {
+  
   return(
     <Router>
       <>
